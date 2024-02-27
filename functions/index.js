c03475b00eac50d84da5f1d1d4b5e88872b0bb6b
Meghan Leicht <mleicht92@gmail.com>
2024-02-27 14:10:48 -0800

    set up dialogflow webhook

---
+++ functions/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+const { WebhookClient } = require("dialogflow-fulfillment");
+
+app.get("/", (req, res) => {
+  res.send(`It's alive!`);
+});
+
+app.post("/", express.json(), (req, res) => {
+  const agent = new WebhookClient({
+    request: req,
+    response: res,
+  });
+
+  function welcome(agent) {
+    agent.add(`Welcome to my agent!`);
+  }
+
+  function fallback(agent) {
+    agent.add(`I didn't understand`);
+    agent.add(`I'm sorry, can you try again?`);
+  }
+
+  function dailyAffirmation(agent) {
+    agent.add(`This is the daily affirmation intent!`);
+  }
+
+  let intentMap = new Map();
+  intentMap.set("Default Welcome Intent", welcome);
+  intentMap.set("Default Fallback Intent", fallback);
+  intentMap.set("Daily Affirmation Intent", dailyAffirmation);
+  agent.handleRequest(intentMap);
+});
+
+app.listen(PORT, () =>
+  console.log(`It's alive! Server is running on http://localhost:${PORT}`)
+);
